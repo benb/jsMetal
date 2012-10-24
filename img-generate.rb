2021e7b65517e0dce9c6d61d842771ea637d540e
Ben Blackburne <bpb@lysozyme.net>
2012-10-24 16:28:15 +0100

    Correct pngs

--- img-generate.rb
+++ img-generate.rb
@@ -1,10 +1,11 @@
 #!/usr/bin/env ruby
 require 'chunky_png'
 png = ChunkyPNG::Image.new(100, 10, ChunkyPNG::Color('gray'))
+png.save('0.png')
 (0...100).each do |i| 
         (0...10).each do |j|
                 png[i,j]=ChunkyPNG::Color('red')
         end
-        png.save(i.to_s + '.png')
+        png.save((i+1).to_s + '.png')
 end
 
